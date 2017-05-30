e16dae1436c57e6053c852d35b1f99e8cf13fb6c
Garrett Kelly Stocksdale <gstocksd@purdue.edu>
2017-05-30 08:20:59 -0400

    Initial Commit

---
+++ soundBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundBoard
+//
+//  Created by Garrett Kelly Stocksdale on 5/30/17.
+//  Copyright © 2017 Garrett Kelly Stocksdale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
